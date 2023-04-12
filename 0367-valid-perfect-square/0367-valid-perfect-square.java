99e899d49e021f09c43cf4dfe7d11905045945ac
satwik-sengar113 <127686255+satwik-sengar113@users.noreply.github.com>
2023-04-12 09:47:05 +0530

    Time: 0 ms (100.00%), Space: 39.1 MB (84.27%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        long lo=0,hi=num;
+        
+        while(lo<=hi){
+            long mid=lo+(hi-lo)/2;
+            if(mid*mid==num)
+                return true;
+            else if(mid*mid<num){
+                lo=mid+1;
+            }
+            else{
+                hi=mid-1;
+            
+            }
+        }
+        return false;
+    }
+}
